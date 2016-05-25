d231103f055ea5a4f38e966a4c2f0f7200af276e
chrupeq <chrupeq@chrupeqPc>
2016-05-25 18:00:29 +0100

    The Message for commit

---
+++ src/main/java/org/arquillian/example/NameOfYourProject/RegistryApplication.java
@@ -0,0 +1,9 @@
+package org.arquillian.example.NameOfYourProject;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("registry")
+public class RegistryApplication extends Application {
+
+}
